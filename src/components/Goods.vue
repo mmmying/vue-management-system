f19349ddd89da004b8ff2e3bb239732551a9a5ca
mmmying <987042638@qq.com>
2020-09-26 19:27:55 +0800

    商品列表

--- src/components/Goods.vue
+++ src/components/Goods.vue
@@ -1,3 +1,132 @@
 <template>
-    <div>goods</div>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <el-input
+          placeholder="请输入内容"
+          prefix-icon="el-icon-search"
+          v-model="searchKey"
+          @keyup.enter.native="queryGoodList"
+        ></el-input>
+      </el-col>
+      <el-col :span="12">
+        <el-button type="primary" @click="addGood()">添加商品</el-button>
+      </el-col>
+    </el-row>
+    <el-table :data="tableData" stripe border style="width: 100%">
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="goods_name" label="商品名称"></el-table-column>
+      <el-table-column
+        prop="goods_price"
+        label="商品价格(元)"
+        width="180"
+      ></el-table-column>
+      <el-table-column
+        prop="goods_weight"
+        label="商品重量(克)"
+        width="180"
+      ></el-table-column>
+      <el-table-column prop="add_time" label="创建时间" width="180">
+        <template slot-scope="scope">
+          {{ scope.row.add_time | formatDate }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="operation" label="操作" width="180">
+        <template slot-scope="scope">
+          <operation-btns
+            :data="scope.row"
+            @editData="editUser"
+            @deleteData="deleteUser"
+          ></operation-btns>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagination.pageindex"
+      :page-sizes="[10, 20, 30, 50]"
+      :page-size="pagination.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
+  </div>
 </template>
+
+<script>
+import OperationBtns from './common/OperationBtns.vue';
+export default {
+  data() {
+    return {
+      tableData: [],
+      pagination: {
+        pagesize: 10,
+        pageindex: 1,
+      },
+      total: 0,
+      searchKey: '',
+    };
+  },
+  components: { OperationBtns },
+  mounted() {
+    this.queryGoodList();
+  },
+  methods: {
+    queryGoodList() {
+      this.$axios
+        .get('goods', {
+          params: {
+            query: this.searchKey,
+            pagenum: this.pagination.pageindex,
+            pagesize: this.pagination.pagesize,
+          },
+        })
+        .then(({ data: res }) => {
+          this.tableData = res.data.goods;
+          this.total = res.data.total;
+        });
+    },
+    handleSizeChange(val) {
+      this.pagination.pagesize = val;
+      this.pagination.pageindex = 1;
+      this.queryGoodList();
+    },
+    handleCurrentChange(val) {
+      this.pagination.pageindex = val;
+      this.queryGoodList();
+    },
+    addGood() {},
+    editUser(row) {},
+    deleteUser(row) {
+      this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }).then(() => {
+        this.$axios
+          .delete(`users/${row.id}`)
+          .then(({ data: res }) => {
+            if (res.meta.status === 200) {
+              this.$message.success('删除成功');
+              if (
+                (this.pagination.pageindex - 1) * this.pagination.pagesize +
+                  1 ===
+                this.total
+              ) {
+                this.pagination.pageindex -= 1;
+              }
+              this.queryGoodList();
+            } else {
+              this.$message.error(res.meta.msg);
+            }
+          })
+          .catch(() => {
+            this.$message.error('删除失败');
+          });
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios'
-import './assets/css/global.css'
-import './assets/fonts/iconfont.css'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import axios from 'axios';
+import './assets/css/global.css';
+import './assets/fonts/iconfont.css';
 import {
   Button,
   Input,
@@ -32,46 +32,57 @@ import {
   Select,
   Option,
   MessageBox
-} from 'element-ui'
+} from 'element-ui';
 // Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 }; // 全局配置
-Vue.use(Button)
-Vue.use(Input)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(Container)
-Vue.use(Header)
-Vue.use(Aside)
-Vue.use(Main)
-Vue.use(Footer)
-Vue.use(Menu)
-Vue.use(Submenu)
-Vue.use(MenuItem)
-Vue.use(Divider)
-Vue.use(Breadcrumb)
-Vue.use(Pagination)
-Vue.use(Table)
-Vue.use(TableColumn)
-Vue.use(Switch)
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(Dialog)
-Vue.use(Select)
-Vue.use(Option)
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Footer);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(Divider);
+Vue.use(Breadcrumb);
+Vue.use(Pagination);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Switch);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Dialog);
+Vue.use(Select);
+Vue.use(Option);
 
-Vue.prototype.$message = Message
-Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$message = Message;
+Vue.prototype.$confirm = MessageBox.confirm;
 
-Vue.prototype.$message = Message
+Vue.prototype.$message = Message;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
 // 请求拦截器
 axios.interceptors.request.use(config => {
-  config.headers.Authorization = window.sessionStorage.getItem('token')
-  return config
-})
-Vue.prototype.$axios = axios
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+});
+Vue.prototype.$axios = axios;
+
+Vue.filter('formatDate', function(val) {
+  const date = new Date(val);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1 + '').padStart(2, '0');
+  const day = (date.getDate() + '').padStart(2, '0');
+  const h = (date.getHours() + '').padStart(2, '0');
+  const m = (date.getMinutes() + '').padStart(2, '0');
+  const s = (date.getSeconds() + '').padStart(2, '0');
+  return `${year}-${month}-${day} ${h}:${m}:${s}`;
+});
 
 /* eslint-disable no-new */
 new Vue({
@@ -79,4 +90,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
